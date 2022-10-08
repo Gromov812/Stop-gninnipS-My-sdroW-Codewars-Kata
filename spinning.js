b7e1d0a39e076690e1721277452a26101bb1d80d
Nikita Gromov <105276303+Gromov812@users.noreply.github.com>
2022-10-08 14:42:41 +0500

    Create spinning.js

---
+++ spinning.js
@@ -0,0 +1,3 @@
+function spinWords(str){
+ return str.split(" ").map((item,index) => item = [item]).map((item,index) => item = item[0].length > 4 ? item.join("").split("").reverse().join("").split(","): item).join(" ");
+}
